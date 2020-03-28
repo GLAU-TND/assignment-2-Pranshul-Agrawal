630d58f70e4dbba1f895b8c6ae500d48c324028f
PRANSHUL AGRAWAL <pranshul.agrawal_cs18@gla.ac.in>
2020-03-28 14:21:55 +0530

    main method

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -5,7 +5,21 @@
  *  Time: 7:17 PM
  */
 package problem1.main;
+import problem1.mybst.MyBinarySearchTree;
+
 // executable class
 
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        tree.insert(25);
+        tree.insert(15);
+        tree.insert(35);
+        tree.insert(11);
+        tree.insert(20);
+        tree.insert(34);
+        tree.insert(40);
+
+        tree.ourTraversal();
+    }
 }
