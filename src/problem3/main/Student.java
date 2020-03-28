0c7da86a64019220e90f0752de38a5d321ef8f14
PRANSHUL AGRAWAL <pranshul.agrawal_cs18@gla.ac.in>
2020-03-28 14:59:30 +0530

    constructor

--- src/problem3/main/Student.java
+++ src/problem3/main/Student.java
@@ -4,4 +4,10 @@ public class Student {
     private String firstName;
     private String lastName;
     private int roll;
+    public Student(String firstName, String lastName, int roll) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.roll = roll;
+    }
+
 }
