a292a745b8f5df75fda5ce020f8bbc36a64b8c7e
PRANSHUL AGRAWAL <pranshul.agrawal_cs18@gla.ac.in>
2020-03-28 16:28:20 +0530

    private variables

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -1,5 +1,15 @@
 package problem4.myqueue;
+import com.sun.source.tree.Tree;
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+import problem4.element.Element;
 
-public class MyQueue {
+import java.util.LinkedList;
+import java.util.List;
+
+public class MyQueue <T>{
+    private Element front;
+    private Element back;
+    private MyBinarySearchTree tree;
 
 }
